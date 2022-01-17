dba20a97ad763f0f75ca00964464ee199a06ce42
oviecodes <alecgee73@gmail.com>
2022-01-17 15:19:59 +0100

    update controller

--- src/api/ticket/controllers/ticket.js
+++ src/api/ticket/controllers/ticket.js
@@ -18,28 +18,16 @@ module.exports = createCoreController('api::ticket.ticket', ({ strapi }) => ({
             populate: "tickets"
         })
 
-        // console.log(event.tickets.length)
-
-        // console.log(event.tickets_available - event.tickets.length)
-
-        // console.log()
-
         if(ctx.request.body.data.total_seats > event.tickets_available) {
             return ctx.badRequest('Cannot book ticket at the moment')
         }
-
-        // const response = await super.create(ctx);
 
         const response = await strapi.service('api::ticket.ticket').create(ctx.request.body)
 
         const event_update = await strapi.service('api::event.event').update(event_id, { data: {
             tickets_available: event.tickets_available - ctx.request.body.data.total_seats
         }})
-        // some more logic
-
-        console.log('response', response)
-        console.log('here', event_update)
-      
+        
         return response;
       }
       
